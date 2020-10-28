8d0869de56dfc4edc7c16896f1fc7bd279cb2c32
Harley <youremail@example.com>
2020-10-28 12:30:03 -0400

    added search bar and background color

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
+let main = $('main') 
+let nasaData; 
+
 $.ajax('https://api.nasa.gov/planetary/apod?api_key=AzFJGwKfWFx8g0pdrerNqnov5QcSwLELz1ctz0od&date=2020-10-01')
 .then(function(data) {
-  console.log(data)
+  nasaData = data;
+  render()
 },
+
 function(error) {
   console.log(error)
 });
+
+
+function render(){
+let img = $('<img>')
+img.attr('src', nasaData.hdurl)
+main.html(img)
+}
 
