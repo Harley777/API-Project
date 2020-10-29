b54750d2f793265fbebcb0162d9833867744697f
Harley <youremail@example.com>
2020-10-29 13:58:03 -0400

    Made date search bar functional

--- README.md
+++ README.md
@@ -11,5 +11,12 @@ Get a daily image from space or search for a image by date
 - Jquery 
 - NASA API
 
+# Link to the website
+[Click here](https://harley777.github.io/) to see the page!
+
 # Screenshots 
 ![wireframe](spaceimagewireframe.png)
+
+# Future Enhancements 
+Get explanations of the Photos to show up next to them
+Have the current days photo appear next to the searched for photo 
--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
+
+
+let button = $('button')
 let main = $('main') 
 let nasaData; 
+let input = $('input')
 
-$.ajax('https://api.nasa.gov/planetary/apod?api_key=AzFJGwKfWFx8g0pdrerNqnov5QcSwLELz1ctz0od&date=2020-10-01')
+button.on('click', function(evt) {
+  $.ajax('https://api.nasa.gov/planetary/apod?api_key=AzFJGwKfWFx8g0pdrerNqnov5QcSwLELz1ctz0od&date='+input.val())
+  .then(function(data) {
+    nasaData = data;
+    render()
+  },
+  
+  function(error) {
+    console.log(error)
+  });
+  
+})
+
+
+$.ajax('https://api.nasa.gov/planetary/apod?api_key=AzFJGwKfWFx8g0pdrerNqnov5QcSwLELz1ctz0od')
 .then(function(data) {
   nasaData = data;
   render()
